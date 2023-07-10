d1c591424a948e148e6c37b33e54a5cc800b98d0
Burlet Médéric <mburlet@palo-it.com>
2023-07-10 18:53:45 +0800

    [POU-86] adding search widget

--- src/components/molecules/RegionSearch.tsx
+++ src/components/molecules/RegionSearch.tsx
@@ -1,28 +1,41 @@
 import { Regions } from "@/@types/Region"
 import { Flex, Text, Box, Divider, HStack, Tag, Button, TagLabel } from "@chakra-ui/react"
-import { useState } from "react"
+import { ReactNode, useState } from "react"
+
+const RegionSearchTitle = (region: string | undefined) => {
+
+
+}
 
 export const RegionSearch = (): React.JSX.Element => {
     const [region, setRegion] = useState<string | undefined>(undefined)
+    //TODO: Finish design of round button and colors of tags
+    const regionTitle = () => {
+        if (region) {
+            return (<Flex alignItems={"center"}>
+                <Button borderRadius={"50%"} backgroundColor={"transparent"} onClick={() => setRegion(undefined)}>&lt;</Button>
+                <Text fontWeight={"bold"}>{region}</Text>
+            </Flex>)
+        } else {
+            return <Box><Text fontWeight={"bold"}>Region</Text></Box>
+        }
+    }
     return (
         <Flex height={"40px"} alignItems={"center"}>
-            {region
-                ? <Flex alignItems={"center"}><Button>&lt;</Button><Text>{region}</Text></Flex>
-                : <Box><Text fontWeight={"bold"}>Region</Text></Box>
-            }
+            {regionTitle()}
             <Divider marginX={6} height={"70%"} orientation="vertical" />
             <HStack spacing={4}>
-                {Regions.map((region) => (
-                    <Button variant={"unstyled"} onClick={() => setRegion(region)}>
+                {Regions.map((regionName) => (regionName !== region &&
+                    <Button variant={"unstyled"} onClick={() => setRegion(regionName)}>
 
                         <Tag
                             size={"md"}
-                            key={region}
+                            key={regionName}
                             borderRadius='full'
                             variant='solid'
                             colorScheme='gray'
                         >
-                            <TagLabel>{region}</TagLabel>
+                            <TagLabel>{regionName}</TagLabel>
                         </Tag>
                     </Button>
 
