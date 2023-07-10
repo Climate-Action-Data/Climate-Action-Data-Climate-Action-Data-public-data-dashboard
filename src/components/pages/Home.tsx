551a346921ccfcc3ca6b1383df750b2146d74d3d
Burlet Médéric <mburlet@palo-it.com>
2023-07-10 19:05:32 +0800

    [POU-86] adding timeframe search

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Flex, Container, Spacer } from "@chakra-ui/react";
 import { NextPage } from "next";
 import { RegionSearch } from "../molecules/RegionSearch";
+import { TimeframeSearch } from "../molecules/TimeframeSearch";
 
 
 export const Home: NextPage = () => {
@@ -8,6 +9,7 @@ export const Home: NextPage = () => {
         <Flex>
             <Container marginTop={"40px"} flex={1} variant="cardSection" >
                 <RegionSearch />
+                <TimeframeSearch />
                 test
             </Container>
         </Flex>
