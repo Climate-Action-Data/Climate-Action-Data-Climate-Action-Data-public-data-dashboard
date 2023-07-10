d14cf7077a3a77c0c8662da32bc114bfd2e9f413
Burlet Médéric <mburlet@palo-it.com>
2023-07-10 16:56:53 +0800

    [POU-86] Setup base menu

---
+++ src/overmind/analytics/actions.ts
@@ -0,0 +1,14 @@
+import { Context } from '..';
+
+interface SearchParams {
+    region: string | null
+    timeframe: string | null
+}
+
+export const getCarbonReduction = async (context: Context, searchParams: SearchParams = { region: null, timeframe: null }) => {
+
+    const carbonData = await context.effects.analytics.getCarbonReduction()
+    if (carbonData) {
+        context.state.analytics.carbonReduction = carbonData
+    }
+}
---
+++ src/overmind/analytics/index.ts
@@ -0,0 +1,6 @@
+// Importing necessary modules
+import { state } from './state'; 
+import * as actions from './actions';
+import * as effects from './effects';
+// Export modules
+export default { state, actions,effects };
