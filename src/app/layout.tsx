d59f6bdcb902e616ed62c13fcb2659b8ad1e6f80
Ming Liang <kmliang@palo-it.com>
2023-07-11 16:03:07 +0800

    [POU-86] Implemented page indicators on menu
    - restructured the icon files
    - added gitignore for IntelliJ configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,69 +13,66 @@ import { Header } from '../components/organisms/Header'
 import { brandPrimary } from '@/styles/Button'
 import { Menu } from '@/components/organisms/Menu'
 import { Home } from '../components/pages/Home'
-import { mode } from "@chakra-ui/theme-tools";
+import { mode } from '@chakra-ui/theme-tools'
 import { cardSection } from '@/styles/Section'
 import styles from './page.module.css'
 
-
-
 const inter = Inter({ subsets: ['latin'] })
-
-
 
 const theme = extendTheme({
   components: {
     Button: {
       variants: {
-        brandPrimary
+        brandPrimary,
+      },
+    },
+    Link: {
+      variants: {
+        brandPrimary,
       },
     },
     Container: {
       variants: {
-        cardSection
-      }
-    }
+        cardSection,
+      },
+    },
   },
   colors: {
     gray: {
-      main: "#00242C",
-      50: "#3E4A4E",
-      100: "#3E4A4E",
-      200: "#3E4A4E",
-      300: "#2E373B",
-      400: "#2E373B",
-      500: "#00242C",
-      600: "#00242C",
-      700: "#00242C",
-      800: "#00242C",
-      900: "#00242C"
+      main: `#00242C`,
+      50: `#3E4A4E`,
+      100: `#3E4A4E`,
+      200: `#3E4A4E`,
+      300: `#2E373B`,
+      400: `#2E373B`,
+      500: `#00242C`,
+      600: `#00242C`,
+      700: `#00242C`,
+      800: `#00242C`,
+      900: `#00242C`,
     },
     green: {
-      main: "#2DEC7C",
-      50: "#EAFDF2",
-      100: "#D5FBE5",
-      200: "#2DEC7C",
-      300: "#2DEC7C",
-      400: "#2DEC7C",
-      500: "#2DEC7C",
-      600: "#24BD63",
-      700: "#1B8E4A",
-      800: "#125E32",
-      900: "#364D3F"
-    }
+      main: `#2DEC7C`,
+      50: `#EAFDF2`,
+      100: `#D5FBE5`,
+      200: `#2DEC7C`,
+      300: `#2DEC7C`,
+      400: `#2DEC7C`,
+      500: `#2DEC7C`,
+      600: `#24BD63`,
+      700: `#1B8E4A`,
+      800: `#125E32`,
+      900: `#364D3F`,
+    },
   },
   styles: {
     global: (props: any) => ({
       body: {
-        bg: mode(
-          "#F8FAFA",
-          "#000"
-        )(props),
+        bg: mode(`#F8FAFA`, `#000`)(props),
       },
     }),
   },
-});
-
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
@@ -95,6 +92,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
           </NoSSR>
         </body>
       </html>
-    </Provider >
+    </Provider>
   )
 }
