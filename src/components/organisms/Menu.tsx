7c52d952b933448316ccde9291a4bee8e708726a
Burlet Médéric <mburlet@palo-it.com>
2023-07-11 15:33:47 +0800

    [POU-86] Icon formatting

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -1,30 +1,34 @@
 import React from 'react'
-import hamburgerIcon from './../../assets/icons/hamburger.svg'
-import HomeIcon from './../../assets/icons/home.svg'
-import BookmarkIcon from './../../assets/icons/bookmark.svg'
-import WebRedirectIcon from './../../assets/icons/web-redirect.svg'
 import Logo from './../../assets/logo.png'
 import Portrait from './../../assets/portrait.png'
-import HelpIcon from './../../assets/icons/help.svg'
-import BellIcon from './../../assets/icons/bell.svg'
 import { Button, Box, Text, Flex, SlideFade, useDisclosure, Image } from '@chakra-ui/react'
-
+import { Link } from '@chakra-ui/next-js'
+import { BellIcon } from '../atoms/BellIcon'
+import { BookmarkIcon } from '../atoms/BookmarkIcon'
+import { HamburgerIcon } from '../atoms/HamburgerIcon'
+import { HelpIcon } from '../atoms/HelpIcon'
+import { WebRedirectIcon } from '../atoms/WebRedirectIcon'
+import { HomeIcon } from '../atoms/HomeIcon'
 export const Menu = (): React.JSX.Element => {
   const { isOpen, onToggle } = useDisclosure()
+
+  const redirect = () => {
+    console.log(`redirect`)
+  }
 
   return (
     <Box className="menu">
       <Flex justifyContent={`space-between`} alignItems={`center`}>
         <Flex alignItems={`center`}>
           <Button position={`sticky`} top={0} colorScheme="gray" width={`56px`} height={`56px`} backgroundColor={isOpen ? `gray.200` : `gray.500`} onClick={onToggle}>
-            <Image alt="Menu" src={hamburgerIcon.src} />
+            <HamburgerIcon color={`white`} />
           </Button>
           <Image marginX={`8px`} alt="Logo" height={`32px`} src={Logo.src} />
         </Flex>
         <Box>Public Data Dashboard</Box>
         <Flex alignItems={`center`}>
           <Button alignSelf={`start`} colorScheme="gray" backgroundColor={`gray.500`} width={`56px`} height={`56px`}>
-            <Image alt="Menu" src={BellIcon.src} />
+            <BellIcon color={`white`} />
           </Button>
           <Button color={`white`} backgroundColor={`gray.500`} display={`flex`} height={`56px`} padding={`16px`} alignItems={`center`}>
             <Text>Hello, Kat</Text>
@@ -32,26 +36,29 @@ export const Menu = (): React.JSX.Element => {
           </Button>
         </Flex>
       </Flex>
-      <SlideFade in={isOpen} style={{ zIndex: 10 }}>
+      <SlideFade in={isOpen} style={{ zIndex: 10, width: `56px` }}>
         <Box color={`white`} backgroundColor={`gray.400`} width={`56px`} height={`100vh`} position={`relative`}>
           <Flex>
-            <Button variant={`brandPrimary`}>
-              <Image alt="Home" src={HomeIcon.src} />
+            <Link as={`button`} variant={`brandPrimary`} href="/">
+              <HomeIcon color={`white`} />
+            </Link>
+            {/* <Button variant={"brandPrimary"} onClick={redirect}>
+                            <Image alt="Home" src={HomeIcon.src} />
+                        </Button> */}
+          </Flex>
+          <Flex>
+            <Button variant={`brandPrimary`} onClick={redirect}>
+              <BookmarkIcon color="white" />
             </Button>
           </Flex>
           <Flex>
-            <Button variant={`brandPrimary`}>
-              <Image alt="Bookmark" src={BookmarkIcon.src} />
+            <Button variant={`brandPrimary`} onClick={redirect}>
+              <WebRedirectIcon color={`white`} />
             </Button>
           </Flex>
           <Flex>
-            <Button variant={`brandPrimary`}>
-              <Image alt="Website" src={WebRedirectIcon.src} />
-            </Button>
-          </Flex>
-          <Flex>
-            <Button variant={`brandPrimary`}>
-              <Image alt="Help" src={HelpIcon.src} />
+            <Button variant={`brandPrimary`} onClick={redirect}>
+              <HelpIcon color={`white`} />
             </Button>
           </Flex>
         </Box>
