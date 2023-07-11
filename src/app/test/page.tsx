02da6cbd701cedd7771c749476ea09abbabc194b
Burlet Médéric <mburlet@palo-it.com>
2023-07-11 12:00:18 +0800

    [POU-86] Functional routing

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { type NextPage } from "next";
+
+const Test: NextPage = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>data</p>
+        </div>
+    )
+}
+
+export default Test
