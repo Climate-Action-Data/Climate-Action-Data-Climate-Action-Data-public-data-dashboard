b2ddfbb24a440a81566e9001a7d665e836b921f6
Burlet Médéric <mburlet@palo-it.com>
2023-07-12 16:53:02 +0800

    [POU-86] Updating icon structure

--- src/components/atoms/icons/__tests__/HelpIcon.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import { HelpIcon } from '../HelpIcon';
-
-it('renders correctly', () => {
-    const { container } = render(<HelpIcon />)
-    expect(container).toMatchSnapshot();
-});
--- src/components/atoms/icons/__tests__/HomeIcon.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import { HomeIcon } from '../HomeIcon';
-
-it('renders correctly', () => {
-    const { container } = render(<HomeIcon />)
-    expect(container).toMatchSnapshot();
-});
