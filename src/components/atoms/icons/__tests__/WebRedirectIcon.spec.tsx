6a5a1c9d3a2d30448f241c73fe9dcb84fdc83abf
Burlet Médéric <mburlet@palo-it.com>
2023-07-12 16:53:02 +0800

    [POU-86] Updating icon structure

--- src/components/atoms/icons/__tests__/WebRedirectIcon.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import { WebRedirectIcon } from '../WebRedirectIcon';
-
-it('renders correctly', () => {
-    const { container } = render(<WebRedirectIcon />)
-    expect(container).toMatchSnapshot();
-});
