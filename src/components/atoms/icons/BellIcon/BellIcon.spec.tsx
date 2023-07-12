941ceca6fad754871f40e6bf34cd40bca5653813
Burlet Médéric <mburlet@palo-it.com>
2023-07-12 16:53:02 +0800

    [POU-86] Updating icon structure

--- src/components/atoms/icons/BellIcon/BellIcon.spec.tsx
+++ src/components/atoms/icons/BellIcon/BellIcon.spec.tsx
@@ -1,7 +1,9 @@
+'use strict';
+
 import { render } from '@testing-library/react'
-import { HamburgerIcon } from '../HamburgerIcon';
+import { BellIcon } from './BellIcon';
 
 it('renders correctly', () => {
-    const { container } = render(<HamburgerIcon />)
+    const { container } = render(<BellIcon />)
     expect(container).toMatchSnapshot();
 });
