1dcf49e87c1299e77e7a418d322b7bfb43f6da2e
Burlet Médéric <mburlet@palo-it.com>
2023-07-12 16:53:02 +0800

    [POU-86] Updating icon structure

--- src/components/atoms/icons/HelpIcon/HelpIcon.spec.tsx
+++ src/components/atoms/icons/HelpIcon/HelpIcon.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import { HelpIcon } from '../HelpIcon';
+import { HelpIcon } from './HelpIcon';
 
 it('renders correctly', () => {
     const { container } = render(<HelpIcon />)
