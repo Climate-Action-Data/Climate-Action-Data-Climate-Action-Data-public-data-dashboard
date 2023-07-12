450e1f82338b9494ad9f63f0c6833c437638a1e5
Burlet Médéric <mburlet@palo-it.com>
2023-07-12 16:53:02 +0800

    [POU-86] Updating icon structure

--- src/components/atoms/icons/__tests__/HamburgerIcon.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react'
-import { HamburgerIcon } from '../HamburgerIcon';
-
-it('renders correctly', () => {
-    const { container } = render(<HamburgerIcon />)
-    expect(container).toMatchSnapshot();
-});
