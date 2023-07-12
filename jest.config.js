a16fead64684c2a890db299fc4422bb75170b082
Burlet Médéric <mburlet@palo-it.com>
2023-07-12 15:12:46 +0800

    [POU-86] Testing sonar jest coverage

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,13 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
     testEnvironment: 'jest-environment-jsdom',
+    reporters: [
+        'default',
+        ['jest-sonar', {
+            outputDirectory: '.',
+            outputName: 'test-report.xml'
+        }]
+    ]
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
