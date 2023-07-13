f558370ec92ce5182abf2997727458e86962e368
Burlet Médéric <mburlet@palo-it.com>
2023-07-13 09:24:30 +0800

    feat: design of widget

--- src/@types/Timeframe.ts
+++ src/@types/Timeframe.ts
@@ -1,8 +1,8 @@
 export enum Timeframes {
-    MAX = "max",
-    ONE_YEAR = "12m",
-    YEAR_TO_DATE = "ytd",
-    ONE_MONTH = "1m"
+  MAX = `max`,
+  ONE_YEAR = `12m`,
+  YEAR_TO_DATE = `ytd`,
+  ONE_MONTH = `1m`,
 }
 
 export enum TimeframesData {
