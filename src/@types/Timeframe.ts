dec46e3f42c3ab0268091504452eb0834e4eca41
Burlet Médéric <mburlet@palo-it.com>
2023-07-14 10:45:17 +0800

    :recycle: refactor(types): Updating timeframe and effects

--- src/@types/Timeframe.ts
+++ src/@types/Timeframe.ts
@@ -1,6 +1,6 @@
-export const Timeframes = [
-    "max",
-    "12m",
-    "ytd",
-    "1m",
-]
+export enum Timeframes {
+    MAX = "max",
+    ONE_YEAR = "12m",
+    YEAR_TO_DATE = "ytd",
+    ONE_MONTH = "1m"
+}
