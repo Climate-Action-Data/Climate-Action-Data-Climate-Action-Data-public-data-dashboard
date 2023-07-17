81b93737de006164e4e3c29fce3a572c9e85a63f
Burlet Médéric <mburlet@palo-it.com>
2023-07-17 18:11:17 +0800

    :sparkles: feat(map): Adding filter to map

--- src/overmind/analytics/state.ts
+++ src/overmind/analytics/state.ts
@@ -1,6 +1,26 @@
 import { EffectResponse } from '@/@types/EffectResponse'
+import { TimeframesData } from '@/@types/Timeframe'
+import { SubRegion } from '@/@types/geojson'
+
+interface CarbonData {
+  activeProjects: number
+  totalReductions: number
+  estimatedReductions: number
+  unitMetric: string
+  sectors: { name: string; average: number }[]
+  standards: { name: string; average: number }[]
+}
+
+export interface CarbonMapData {
+  countryCode: string
+  timeRanges: Record<TimeframesData, CarbonData>
+}
 
 export interface CarbonReduction {
+  carbonMapData: CarbonMapData[]
+  carbonMapHasCountryData: Map<string, boolean>
+  carbonMapSelectedRegion?: SubRegion
+  carbonMapHoveredRegion?: string
   activeProjects: number
   totalReduction: number
   annualEstReduction: number
