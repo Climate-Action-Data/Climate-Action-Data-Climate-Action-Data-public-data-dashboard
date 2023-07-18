a3b2518d1ee9179cd38870ba9f6b4ecb5a9cf2e2
Burlet Médéric <mburlet@palo-it.com>
2023-07-18 11:26:20 +0800

    :bug: fix(alt): Localising alt text

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,10 +1,13 @@
 import { Box, Image } from '@chakra-ui/react'
 import headerBanner from './../../../assets/header.jpg'
+import { useTranslation } from 'react-i18next'
 
 export const Header = (): React.JSX.Element => {
+  const { t } = useTranslation(`home`)
+
   return (
     <Box>
-      <Image alt="CAD Banner" src={headerBanner.src} />
+      <Image alt={t(`images.bannerImage`)} src={headerBanner.src} />
     </Box>
   )
 }
