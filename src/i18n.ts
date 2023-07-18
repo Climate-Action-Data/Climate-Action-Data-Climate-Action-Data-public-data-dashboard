03c97a4ee08d6e898f1cdef528299ca69d9d5e0f
Ming Liang <kmliang@palo-it.com>
2023-07-18 13:30:45 +0800

    🌐 chore: comments from PR

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,8 @@
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
+
 import enHome from './locales/en/home.json'
+import enMenu from './locales/en/menu.json'
 
 i18next.use(initReactI18next).init({
   debug: true,
@@ -8,6 +10,7 @@ i18next.use(initReactI18next).init({
   resources: {
     en: {
       home: enHome,
+      menu: enMenu,
     },
   },
 })
