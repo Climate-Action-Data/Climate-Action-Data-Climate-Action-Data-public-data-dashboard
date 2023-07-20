7dde265e4480b68530f7f95188de494ee59473f2
Burlet Médéric <mburlet@palo-it.com>
2023-07-20 16:04:16 +0800

    :bug: fix(sonar): Fixing sonar cloud bugs

---
+++ src/assets/map_dashboard_data.ts
@@ -0,0 +1,438 @@
+const countryData = [
+  {
+    countryCode: `TUR`,
+    timeRanges: {
+      max: {
+        activeProjects: 200,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+    },
+  },
+  {
+    countryCode: `NPL`,
+    timeRanges: {
+      max: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+    },
+  },
+  {
+    countryCode: `BRA`,
+    timeRanges: {
+      max: {
+        activeProjects: 100,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+    },
+  },
+  {
+    countryCode: `GHA`,
+    timeRanges: {
+      max: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+    },
+  },
+  {
+    countryCode: `BOL`,
+    timeRanges: {
+      max: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [
+          {
+            name: `Agriculture; forestry and fishing`,
+            average: 46,
+          },
+          {
+            name: `Agriculture, forestry and other land uses (AFOLU)`,
+            average: 36,
+          },
+          {
+            name: `Electricity; gas, steam and air conditioning supply`,
+            average: 13,
+          },
+          {
+            name: `Others`,
+            average: 5,
+          },
+        ],
+        standards: [],
+      },
+    },
+  },
+  {
+    countryCode: `CHL`,
+    timeRanges: {
+      max: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [
+          {
+            name: `Agriculture; forestry and fishing`,
+            average: 46,
+          },
+          {
+            name: `Agriculture, forestry and other land uses (AFOLU)`,
+            average: 36,
+          },
+          {
+            name: `Electricity; gas, steam and air conditioning supply`,
+            average: 13,
+          },
+          {
+            name: `Others`,
+            average: 5,
+          },
+        ],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+    },
+  },
+  {
+    countryCode: `COL`,
+    timeRanges: {
+      max: {
+        activeProjects: 78,
+        totalReductions: 140714016,
+        estimatedReductions: 238757705,
+        unitMetric: `tCO2e`,
+        sectors: [
+          {
+            name: `Agriculture; forestry and fishing`,
+            average: 56,
+          },
+          {
+            name: `Agriculture, forestry and other land uses (AFOLU)`,
+            average: 26,
+          },
+          {
+            name: `Electricity; gas, steam and air conditioning supply`,
+            average: 13,
+          },
+          {
+            name: `Others`,
+            average: 5,
+          },
+        ],
+        standards: [
+          {
+            name: `EcoRegistry`,
+            average: 74,
+          },
+          {
+            name: `Biocarbon Registry S.A.S`,
+            average: 26,
+          },
+        ],
+      },
+      twelveMonths: {
+        activeProjects: 78,
+        totalReductions: 140714016,
+        estimatedReductions: 238757705,
+        unitMetric: `tCO2e`,
+        sectors: [
+          {
+            name: `Agriculture; forestry and fishing`,
+            average: 56,
+          },
+          {
+            name: `Agriculture, forestry and other land uses (AFOLU)`,
+            average: 26,
+          },
+          {
+            name: `Electricity; gas, steam and air conditioning supply`,
+            average: 13,
+          },
+          {
+            name: `Others`,
+            average: 5,
+          },
+        ],
+        standards: [
+          {
+            name: `EcoRegistry`,
+            average: 74,
+          },
+          {
+            name: `Biocarbon Registry S.A.S`,
+            average: 26,
+          },
+        ],
+      },
+      sixMonths: {
+        activeProjects: 78,
+        totalReductions: 140714016,
+        estimatedReductions: 238757705,
+        unitMetric: `tCO2e`,
+        sectors: [
+          {
+            name: `Agriculture; forestry and fishing`,
+            average: 56,
+          },
+          {
+            name: `Agriculture, forestry and other land uses (AFOLU)`,
+            average: 26,
+          },
+          {
+            name: `Electricity; gas, steam and air conditioning supply`,
+            average: 13,
+          },
+          {
+            name: `Others`,
+            average: 5,
+          },
+        ],
+        standards: [
+          {
+            name: `EcoRegistry`,
+            average: 74,
+          },
+          {
+            name: `Biocarbon Registry S.A.S`,
+            average: 26,
+          },
+        ],
+      },
+      oneMonth: {
+        activeProjects: 28,
+        totalReductions: 14071016,
+        estimatedReductions: 23857705,
+        unitMetric: `tCO2e`,
+        sectors: [
+          {
+            name: `Agriculture; forestry and fishing`,
+            average: 46,
+          },
+          {
+            name: `Agriculture, forestry and other land uses (AFOLU)`,
+            average: 36,
+          },
+          {
+            name: `Electricity; gas, steam and air conditioning supply`,
+            average: 13,
+          },
+          {
+            name: `Others`,
+            average: 5,
+          },
+        ],
+        standards: [
+          {
+            name: `EcoRegistry`,
+            average: 74,
+          },
+          {
+            name: `Biocarbon Registry S.A.S`,
+            average: 26,
+          },
+        ],
+      },
+    },
+  },
+  {
+    countryCode: `PAN`,
+    timeRanges: {
+      max: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      twelveMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      sixMonths: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+      oneMonth: {
+        activeProjects: 0,
+        totalReductions: 0,
+        estimatedReductions: 0,
+        unitMetric: `tCO2e`,
+        sectors: [],
+        standards: [],
+      },
+    },
+  },
+]
+
+export default countryData
