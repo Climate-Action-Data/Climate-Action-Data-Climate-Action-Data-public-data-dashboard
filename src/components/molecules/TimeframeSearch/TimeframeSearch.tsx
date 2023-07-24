242ffee2f1068f3dff4399157ad1b6f32eb0d9b6
Burlet Médéric <mburlet@palo-it.com>
2023-07-24 09:44:20 +0800

    :sparkles: feat(timeframe): Removing cross

--- src/components/molecules/TimeframeSearch/TimeframeSearch.tsx
+++ src/components/molecules/TimeframeSearch/TimeframeSearch.tsx
@@ -1,6 +1,6 @@
 import { TimeframesData } from '@/@types/Timeframe'
 import { useActions, useAppState } from '@/overmind'
-import { Flex, HStack, Tag, TagLabel, TagCloseButton } from '@chakra-ui/react'
+import { Flex, HStack, Tag, TagLabel } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 
 export const TimeframeSearch = (): React.JSX.Element => {
@@ -11,26 +11,20 @@ export const TimeframeSearch = (): React.JSX.Element => {
   return (
     <Flex height={`40px`} alignItems={`center`}>
       <HStack spacing={4}>
-        {Object.values(TimeframesData).map(
-          (timeFrameName, idx) =>
-            timeFrameName !== TimeframesData.MAX && (
-              <Tag
-                size={`md`}
-                key={timeFrameName}
-                borderRadius="full"
-                _hover={{ cursor: `pointer` }}
-                variant={timeFrameName === carbonReduction.carbonMapDataFilters.timeframe ? `solid` : `outline`}
-                colorScheme="gray"
-              >
-                <TagLabel data-testid={`button-timeframe-${idx}`} onClick={() => setTimeframe(timeFrameName)}>
-                  {t(`timeframes.${timeFrameName}`)}
-                </TagLabel>
-                {timeFrameName === carbonReduction.carbonMapDataFilters.timeframe && (
-                  <TagCloseButton data-testid={`button-timeframe-close`} onClick={() => setTimeframe(TimeframesData.MAX)} />
-                )}
-              </Tag>
-            ),
-        )}
+        {Object.values(TimeframesData).map((timeFrameName, idx) => (
+          <Tag
+            size={`md`}
+            key={timeFrameName}
+            borderRadius="full"
+            _hover={{ cursor: `pointer` }}
+            variant={timeFrameName === carbonReduction.carbonMapDataFilters.timeframe ? `solid` : `outline`}
+            colorScheme="gray"
+            data-testid={`button-timeframe-${idx}`}
+            onClick={() => setTimeframe(timeFrameName)}
+          >
+            <TagLabel>{t(`timeframes.${timeFrameName}`)}</TagLabel>
+          </Tag>
+        ))}
       </HStack>
     </Flex>
   )
