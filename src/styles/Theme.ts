740d58dc9eef1981ef6f726b0f84d9894e223295
Ming Liang <kmliang@palo-it.com>
2023-07-26 14:58:49 +0800

    💄 feat: added fonts and reused CardSection

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,14 +2,14 @@ import { extendTheme } from '@chakra-ui/react'
 import { mode } from '@chakra-ui/theme-tools'
 
 import { brandPrimary, hoverOnly, lightGray, lightGrayRound, lightGrayRound32, whiteSecondary } from '@/styles/Button'
-import { cardSection, creditHistoryCardSection } from '@/styles/Section'
+import { Aeonik, AeonikFono } from '@/styles/fonts'
+import { Checkbox } from '@/styles/Checkbox'
+import { cardSection } from '@/styles/Section'
 import fontSizes from './FontSizes'
 import { Text } from './Text'
-// import { ellipsis } from './Text'
 import { paginationBar } from './PaginationBar'
 import { searchTable } from './SearchTable'
 import { menuWhite } from './Menu'
-import { Aeonik, AeonikFono } from './fonts'
 
 const theme = extendTheme({
   fontSizes: fontSizes,
@@ -50,7 +50,6 @@ const theme = extendTheme({
     Container: {
       variants: {
         cardSection,
-        creditHistoryCardSection,
         paginationBar,
       },
     },
@@ -60,6 +59,7 @@ const theme = extendTheme({
       },
     },
     Text,
+    Checkbox,
   },
   colors: {
     lightGray: {
