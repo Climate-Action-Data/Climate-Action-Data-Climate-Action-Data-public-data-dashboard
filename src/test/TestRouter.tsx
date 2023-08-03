b4023943c4e1d6aa86f1e39089be64192896c376
Burlet Médéric <mburlet@palo-it.com>
2023-08-03 20:20:55 +0800

    :white_check_mark: test(router): Using memo for test router mocking

--- src/test/TestRouter.tsx
+++ src/test/TestRouter.tsx
@@ -1,5 +1,5 @@
 import { AppRouterContext, AppRouterInstance } from 'next/dist/shared/lib/app-router-context'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 export type TestRouterProps = {
   router: Partial<AppRouterInstance>
@@ -7,14 +7,17 @@ export type TestRouterProps = {
 }
 
 export const TestRouter = ({ router, children }: TestRouterProps): React.ReactNode => {
-  const mockedRouter: AppRouterInstance = {
-    back: jest.fn(),
-    forward: jest.fn(),
-    push: jest.fn(),
-    replace: jest.fn(),
-    refresh: jest.fn(),
-    prefetch: jest.fn(),
-    ...router,
-  }
+  const mockedRouter: AppRouterInstance = useMemo(
+    () => ({
+      back: jest.fn(),
+      forward: jest.fn(),
+      push: jest.fn(),
+      replace: jest.fn(),
+      refresh: jest.fn(),
+      prefetch: jest.fn(),
+      ...router,
+    }),
+    [router],
+  )
   return <AppRouterContext.Provider value={mockedRouter}>{children}</AppRouterContext.Provider>
 }
