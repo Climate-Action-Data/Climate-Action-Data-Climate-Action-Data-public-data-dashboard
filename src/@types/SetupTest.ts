2f1865f3fa1e268f811b9566bf1ce565c25a9fce
Burlet Médéric <mburlet@palo-it.com>
2023-08-03 14:36:23 +0800

    :lipstick: test: Better mock value for API Key

--- src/@types/SetupTest.ts
+++ src/@types/SetupTest.ts
@@ -1,4 +1,4 @@
-process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY = `lqkwjelkqwjelkqwje`
+process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY = `f7f87ee8-16d4-4e99-8aaa-11e36c7edd83`
 
 /* eslint-disable @typescript-eslint/no-empty-function */
 global.matchMedia =
