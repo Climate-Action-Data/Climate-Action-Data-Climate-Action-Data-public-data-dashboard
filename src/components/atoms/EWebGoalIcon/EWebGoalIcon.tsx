8ae09e58e8b9c5af2debb475586cee5934658665
Burlet Médéric <mburlet@palo-it.com>
2023-08-03 13:38:59 +0800

    :recycle: refactor(ewebgoal): Refactor to return tupple

--- src/components/atoms/EWebGoalIcon/EWebGoalIcon.tsx
+++ src/components/atoms/EWebGoalIcon/EWebGoalIcon.tsx
@@ -1,5 +1,6 @@
-import { EWebGoal } from '@/@types/EWebGoal'
 import { Image } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+import { EWebGoal } from '@/@types/EWebGoal'
 
 import EWebGoal1 from '@/assets/web-goal/E-WEB-Goal-01.svg'
 import EWebGoal2 from '@/assets/web-goal/E-WEB-Goal-02.svg'
@@ -18,7 +19,6 @@ import EWebGoal14 from '@/assets/web-goal/E-WEB-Goal-14.svg'
 import EWebGoal15 from '@/assets/web-goal/E-WEB-Goal-15.svg'
 import EWebGoal16 from '@/assets/web-goal/E-WEB-Goal-16.svg'
 import EWebGoal17 from '@/assets/web-goal/E-WEB-Goal-17.svg'
-import { useTranslation } from 'react-i18next'
 
 interface EWebGoalIconProps {
   goal: EWebGoal
@@ -27,40 +27,47 @@ interface EWebGoalIconProps {
 export const EWebGoalIcon = (props: EWebGoalIconProps) => {
   const { goal } = props
   const { t } = useTranslation(`projectDetails`)
-  switch (goal) {
-    case EWebGoal.SDG1:
-      return <Image w="40px" h="40px" src={EWebGoal1.src} alt={t(`coBenefits.SDG1`)} />
-    case EWebGoal.SDG2:
-      return <Image w="40px" h="40px" src={EWebGoal2.src} alt={t(`coBenefits.SDG2`)} />
-    case EWebGoal.SDG3:
-      return <Image w="40px" h="40px" src={EWebGoal3.src} alt={t(`coBenefits.SDG3`)} />
-    case EWebGoal.SDG4:
-      return <Image w="40px" h="40px" src={EWebGoal4.src} alt={t(`coBenefits.SDG4`)} />
-    case EWebGoal.SDG5:
-      return <Image w="40px" h="40px" src={EWebGoal5.src} alt={t(`coBenefits.SDG5`)} />
-    case EWebGoal.SDG6:
-      return <Image w="40px" h="40px" src={EWebGoal6.src} alt={t(`coBenefits.SDG6`)} />
-    case EWebGoal.SDG7:
-      return <Image w="40px" h="40px" src={EWebGoal7.src} alt={t(`coBenefits.SDG7`)} />
-    case EWebGoal.SDG8:
-      return <Image w="40px" h="40px" src={EWebGoal8.src} alt={t(`coBenefits.SDG8`)} />
-    case EWebGoal.SDG9:
-      return <Image w="40px" h="40px" src={EWebGoal9.src} alt={t(`coBenefits.SDG9`)} />
-    case EWebGoal.SDG10:
-      return <Image w="40px" h="40px" src={EWebGoal10.src} alt={t(`coBenefits.SDG10`)} />
-    case EWebGoal.SDG11:
-      return <Image w="40px" h="40px" src={EWebGoal11.src} alt={t(`coBenefits.SDG11`)} />
-    case EWebGoal.SDG12:
-      return <Image w="40px" h="40px" src={EWebGoal12.src} alt={t(`coBenefits.SDG12`)} />
-    case EWebGoal.SDG13:
-      return <Image w="40px" h="40px" src={EWebGoal13.src} alt={t(`coBenefits.SDG13`)} />
-    case EWebGoal.SDG14:
-      return <Image w="40px" h="40px" src={EWebGoal14.src} alt={t(`coBenefits.SDG14`)} />
-    case EWebGoal.SDG15:
-      return <Image w="40px" h="40px" src={EWebGoal15.src} alt={t(`coBenefits.SDG15`)} />
-    case EWebGoal.SDG16:
-      return <Image w="40px" h="40px" src={EWebGoal16.src} alt={t(`coBenefits.SDG16`)} />
-    case EWebGoal.SDG17:
-      return <Image w="40px" h="40px" src={EWebGoal17.src} alt={t(`coBenefits.SDG17`)} />
+
+  const getImageAndText = (goal: EWebGoal) => {
+    switch (goal) {
+      case EWebGoal.SDG1:
+        return [EWebGoal1.src, t(`coBenefits.SDG1`)]
+      case EWebGoal.SDG2:
+        return [EWebGoal2.src, t(`coBenefits.SDG2`)]
+      case EWebGoal.SDG3:
+        return [EWebGoal3.src, t(`coBenefits.SDG3`)]
+      case EWebGoal.SDG4:
+        return [EWebGoal4.src, t(`coBenefits.SDG4`)]
+      case EWebGoal.SDG5:
+        return [EWebGoal5.src, t(`coBenefits.SDG5`)]
+      case EWebGoal.SDG6:
+        return [EWebGoal6.src, t(`coBenefits.SDG6`)]
+      case EWebGoal.SDG7:
+        return [EWebGoal7.src, t(`coBenefits.SDG7`)]
+      case EWebGoal.SDG8:
+        return [EWebGoal8.src, t(`coBenefits.SDG8`)]
+      case EWebGoal.SDG9:
+        return [EWebGoal9.src, t(`coBenefits.SDG9`)]
+      case EWebGoal.SDG10:
+        return [EWebGoal10.src, t(`coBenefits.SDG10`)]
+      case EWebGoal.SDG11:
+        return [EWebGoal11.src, t(`coBenefits.SDG11`)]
+      case EWebGoal.SDG12:
+        return [EWebGoal12.src, t(`coBenefits.SDG12`)]
+      case EWebGoal.SDG13:
+        return [EWebGoal13.src, t(`coBenefits.SDG13`)]
+      case EWebGoal.SDG14:
+        return [EWebGoal14.src, t(`coBenefits.SDG14`)]
+      case EWebGoal.SDG15:
+        return [EWebGoal15.src, t(`coBenefits.SDG15`)]
+      case EWebGoal.SDG16:
+        return [EWebGoal16.src, t(`coBenefits.SDG16`)]
+      case EWebGoal.SDG17:
+        return [EWebGoal17.src, t(`coBenefits.SDG17`)]
+    }
   }
+
+  const [image, altText] = getImageAndText(goal)
+
+  return <Image w="40px" h="40px" src={image} alt={altText} />
 }
