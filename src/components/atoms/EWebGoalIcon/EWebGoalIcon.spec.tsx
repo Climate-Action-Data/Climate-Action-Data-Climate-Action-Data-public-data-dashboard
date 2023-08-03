21d1eebd1a28e3bd6b2dc83b8c66bb9357926d8c
Burlet Médéric <mburlet@palo-it.com>
2023-08-03 13:50:33 +0800

    :white_check_mark: test(ewebgoal): Adding test case for each value of the enum

--- src/components/atoms/EWebGoalIcon/EWebGoalIcon.spec.tsx
+++ src/components/atoms/EWebGoalIcon/EWebGoalIcon.spec.tsx
@@ -2,7 +2,11 @@ import { render } from '@testing-library/react'
 import { EWebGoalIcon } from './EWebGoalIcon'
 import { EWebGoal } from '@/@types/EWebGoal'
 
-it(`renders correctly`, () => {
-  const { container } = render(<EWebGoalIcon goal={EWebGoal.SDG1} />)
-  expect(container).toMatchSnapshot()
+describe(`getImageAndText`, () => {
+  Object.values(EWebGoal).forEach((goal) => {
+    it(`returns correct image and text for ${goal}`, () => {
+      const { container } = render(<EWebGoalIcon goal={goal} />)
+      expect(container).toMatchSnapshot()
+    })
+  })
 })
