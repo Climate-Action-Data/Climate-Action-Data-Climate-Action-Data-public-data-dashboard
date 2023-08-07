e7ee354a69d9d16e965290cdd5fb9f585a6ef979
Sujay <spsurendran@palo-it.com>
2023-08-07 18:29:17 +0800

    :sparkles: feat: Added bread crumb in project details

--- src/components/atoms/ProjectDetailHeader/ProjectDetailHeader.spec.tsx
+++ src/components/atoms/ProjectDetailHeader/ProjectDetailHeader.spec.tsx
@@ -3,15 +3,10 @@ import { ProjectDetailHeader, ProjectDetailHeaderProps } from './ProjectDetailHe
 
 const DEFAULT_PROPS: ProjectDetailHeaderProps = {
   id: `S00123`,
-  type: `type`,
   title: `Project Title`,
-  description: `Project Description`,
-  location: `France`,
 }
 
 it(`renders correctly`, () => {
-  const { container } = render(
-    <ProjectDetailHeader id={DEFAULT_PROPS.id} title={DEFAULT_PROPS.title} location={DEFAULT_PROPS.location} description={DEFAULT_PROPS.description} type={DEFAULT_PROPS.type} />,
-  )
+  const { container } = render(<ProjectDetailHeader id={DEFAULT_PROPS.id} title={DEFAULT_PROPS.title} />)
   expect(container).toMatchSnapshot()
 })
