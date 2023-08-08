f65f37fc0345ef03172716fab100e3ff4c509e43
Burlet Médéric <mburlet@palo-it.com>
2023-08-08 11:01:18 +0800

    :bug: fix(menucontent): no index on key

--- src/components/atoms/MenuContent/MenuContent.tsx
+++ src/components/atoms/MenuContent/MenuContent.tsx
@@ -15,9 +15,9 @@ export const MenuContent = (props: MenuContentProps) => {
   const { menuItems } = props
 
   const generateMenuItems = () => {
-    return menuItems.map((item, index) => {
+    return menuItems.map((item) => {
       return (
-        <MenuItem key={`menu-item-${index}`} data-testid={item.dataTestId} onClick={item.onClick} minH="48px">
+        <MenuItem key={`menu-item-${item.dataTestId}`} data-testid={item.dataTestId} onClick={item.onClick} minH="48px">
           <Text flex={1} as="span">
             {item.text}
           </Text>
