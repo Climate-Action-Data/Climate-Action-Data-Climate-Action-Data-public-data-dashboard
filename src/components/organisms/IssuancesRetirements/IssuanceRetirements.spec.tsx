e48d30edda3d93d049e2aaf6855666d9a7c5027d
Burlet Médéric <mburlet@palo-it.com>
2023-08-14 11:17:10 +0800

    :bug: fix(test): Adding coverage

--- src/components/organisms/IssuancesRetirements/IssuanceRetirements.spec.tsx
+++ src/components/organisms/IssuancesRetirements/IssuanceRetirements.spec.tsx
@@ -7,4 +7,14 @@ describe(`RetirementDetails`, () => {
     const { container } = render(<IssuancesRetirements project={PROJECT_DETAIL} />)
     expect(container).toMatchSnapshot()
   })
+
+  it(`renders correctly project issuances`, () => {
+    const { container } = render(<IssuancesRetirements project={{ ...PROJECT_DETAIL, issuances: [PROJECT_DETAIL.issuances[0]] }} />)
+    expect(container).toMatchSnapshot()
+  })
+
+  it(`renders correctly project issuances`, () => {
+    const { container } = render(<IssuancesRetirements project={{ ...PROJECT_DETAIL, issuances: [] }} />)
+    expect(container).toMatchSnapshot()
+  })
 })
