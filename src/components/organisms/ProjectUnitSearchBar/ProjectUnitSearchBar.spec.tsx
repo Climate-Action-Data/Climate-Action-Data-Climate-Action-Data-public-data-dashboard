84632c82b7651748c39b4f963603f13b72b85108
Burlet Médéric <mburlet@palo-it.com>
2023-08-15 16:30:18 +0800

    :white_check_mark: test(search): Using TestOvermind component

--- src/components/organisms/ProjectUnitSearchBar/ProjectUnitSearchBar.spec.tsx
+++ src/components/organisms/ProjectUnitSearchBar/ProjectUnitSearchBar.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ProjectUnitSearchBar from '@/components/organisms/ProjectUnitSearchBar/ProjectUnitSearchBar'
+import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 
 const mockPush = jest.fn()
 jest.mock(`next/navigation`, () => ({
@@ -10,17 +11,29 @@ jest.mock(`next/navigation`, () => ({
 }))
 
 it(`renders correctly for project search`, () => {
-  const { container } = render(<ProjectUnitSearchBar isProjectSearch={true} />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <ProjectUnitSearchBar isProjectSearch={true} />
+    </TestOvermindWrapper>,
+  )
   expect(container).toMatchSnapshot()
 })
 
 it(`renders correctly for unit search`, () => {
-  const { container } = render(<ProjectUnitSearchBar isProjectSearch={false} />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <ProjectUnitSearchBar isProjectSearch={false} />
+    </TestOvermindWrapper>,
+  )
   expect(container).toMatchSnapshot()
 })
 
 it(`renders correctly for project search with no params, add a search value and submits a search`, async () => {
-  const { container } = render(<ProjectUnitSearchBar isProjectSearch={true} />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <ProjectUnitSearchBar isProjectSearch={true} />
+    </TestOvermindWrapper>,
+  )
   await userEvent.type(screen.getByRole(`textbox`), `hello`)
   await userEvent.click(screen.getByTestId(`search-bar-clear`))
   await userEvent.click(screen.getByTestId(`search-bar-search`))
@@ -29,7 +42,11 @@ it(`renders correctly for project search with no params, add a search value and 
 })
 
 it(`renders correctly and perform a search on enter`, async () => {
-  const { container } = render(<ProjectUnitSearchBar isProjectSearch={false} />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <ProjectUnitSearchBar isProjectSearch={false} />
+    </TestOvermindWrapper>,
+  )
   await userEvent.type(screen.getByRole(`textbox`), `hello`)
   await userEvent.keyboard(`{enter}`)
   expect(mockPush).toHaveBeenCalledWith(`/search/units?keyword=hello`)
