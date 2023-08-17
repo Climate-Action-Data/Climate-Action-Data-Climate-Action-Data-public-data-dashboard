f03ad816715126fd941f2ddf487dbbe26bc00f9c
Burlet Médéric <mburlet@palo-it.com>
2023-08-17 16:09:41 +0800

    :white_check_mark: test(csv): Mocking invisible link

--- src/components/molecules/CSVDownload/CSVDownload.spec.tsx
+++ src/components/molecules/CSVDownload/CSVDownload.spec.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react'
-import { CSVDownload } from './CSVDownload'
+import { CSVDownload, createAndDownload } from './CSVDownload'
 import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 import { CSVExportTypes } from '@/@types/CSV'
 
@@ -40,4 +40,25 @@ it(`renders correctly as pattern and click`, () => {
   const downloadButton = screen.getByTestId(`export-data`)
   fireEvent.click(downloadButton)
   expect(container).toMatchSnapshot()
+})
+
+// Create a test for createAndDownload function in CSVDownload.tsx
+//window.URL.createObjectUR
+const createUrlMock = jest.fn()
+const revokeUrlMock = jest.fn()
+beforeAll(() => {
+  window.URL.createObjectURL = createUrlMock
+  window.URL.revokeObjectURL = revokeUrlMock
+})
+it(`creates a link and downloads`, () => {
+  const testData = new Blob([`test,test2`], { type: `text/csv` })
+  createAndDownload(testData)
+  const invisibleLink = screen.getByTestId(`invisible-link`)
+  expect(createUrlMock).toHaveBeenCalled()
+  expect(revokeUrlMock).toHaveBeenCalled()
+  expect(invisibleLink).toBeDefined()
+})
+
+afterAll(() => {
+  jest.clearAllMocks()
 })
