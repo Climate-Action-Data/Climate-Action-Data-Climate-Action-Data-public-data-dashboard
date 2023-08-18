5af9206132468c0db6bdd3c94008024cfcd5d521
Burlet Médéric <mburlet@palo-it.com>
2023-08-18 14:37:43 +0800

    :bug: fix(column): Fixing inline function

--- src/components/atoms/ColumnSortFilter/ColumnSortFilter.tsx
+++ src/components/atoms/ColumnSortFilter/ColumnSortFilter.tsx
@@ -21,24 +21,18 @@ export const ColumnSortFilter = (props: ColumnSortFilterProps) => {
     return `lightGray.500`
   }
 
-  const handleClick = (direction: DatabaseQueryDirection) => {
-    onClick(sortValue, direction)
+  const handleOnSortAscClick = () => {
+    onClick(sortValue, DatabaseQueryDirection.ASC)
+  }
+
+  const handleOnSortDescClick = () => {
+    onClick(sortValue, DatabaseQueryDirection.DESC)
   }
 
   return (
     <VStack alignItems={`center`} justifyContent="center" gap="2px" height="20px" minW="20px">
-      <SortUpIcon
-        color={isSelected(DatabaseQueryDirection.ASC)}
-        data-testid="sortAsc"
-        onClick={() => handleClick(DatabaseQueryDirection.ASC)}
-        _hover={{ cursor: `pointer`, color: `gray.700` }}
-      />
-      <SortDownIcon
-        color={isSelected(DatabaseQueryDirection.DESC)}
-        data-testid="sortDesc"
-        onClick={() => handleClick(DatabaseQueryDirection.DESC)}
-        _hover={{ cursor: `pointer`, color: `gray.700` }}
-      />
+      <SortUpIcon color={isSelected(DatabaseQueryDirection.ASC)} data-testid="sortAsc" onClick={handleOnSortAscClick} _hover={{ cursor: `pointer`, color: `gray.700` }} />
+      <SortDownIcon color={isSelected(DatabaseQueryDirection.DESC)} data-testid="sortDesc" onClick={handleOnSortDescClick} _hover={{ cursor: `pointer`, color: `gray.700` }} />
     </VStack>
   )
 }
