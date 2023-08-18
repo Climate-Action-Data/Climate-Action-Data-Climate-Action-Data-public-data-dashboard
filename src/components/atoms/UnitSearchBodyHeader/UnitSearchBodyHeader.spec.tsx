20fd92ea519796abfe457648e66b61ed1a529a0c
Burlet Médéric <mburlet@palo-it.com>
2023-08-18 13:52:47 +0800

    :white_check_mark: test(column): Updating test to match new props

--- src/components/atoms/UnitSearchBodyHeader/UnitSearchBodyHeader.spec.tsx
+++ src/components/atoms/UnitSearchBodyHeader/UnitSearchBodyHeader.spec.tsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react'
 import { UnitSearchBodyHeader } from './UnitSearchBodyHeader'
+import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 
 it(`renders correctly`, () => {
-  const { container } = render(<UnitSearchBodyHeader />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <UnitSearchBodyHeader />
+    </TestOvermindWrapper>,
+  )
   expect(container).toMatchSnapshot()
 })
