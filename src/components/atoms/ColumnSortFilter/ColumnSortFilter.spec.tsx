7e4f87604357793486f68a11687431562cd0bc97
Burlet Médéric <mburlet@palo-it.com>
2023-08-18 13:52:47 +0800

    :white_check_mark: test(column): Updating test to match new props

--- src/components/atoms/ColumnSortFilter/ColumnSortFilter.spec.tsx
+++ src/components/atoms/ColumnSortFilter/ColumnSortFilter.spec.tsx
@@ -1,32 +1,37 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import { ColumnSortFilter } from './ColumnSortFilter'
+import { DatabaseQueryDirection, ProjectSearchSortBy } from '@/@types/ProjectSearchFilterValues'
+
+const click = jest.fn()
 
 it(`renders correctly`, () => {
-  const { container } = render(<ColumnSortFilter />)
+  const testProps = { value: ProjectSearchSortBy.NAME, direction: DatabaseQueryDirection.ASC }
+
+  const { container } = render(<ColumnSortFilter sortValue={testProps.value} currentValue={testProps.value} currentDirection={testProps.direction} onClick={click} />)
 
   expect(container).toMatchSnapshot()
 })
 
 it(`renders correctly and click asc`, () => {
-  const sortAsc = jest.fn()
+  const testProps = { value: ProjectSearchSortBy.RELEVANCE, direction: DatabaseQueryDirection.ASC }
 
-  const { container } = render(<ColumnSortFilter onSortAsc={() => sortAsc()} />)
+  const { container } = render(<ColumnSortFilter sortValue={testProps.value} currentValue={testProps.value} currentDirection={testProps.direction} onClick={click} />)
 
   const inputElement = screen.getByTestId(`sortAsc`)
   fireEvent.click(inputElement)
 
   expect(container).toMatchSnapshot()
-  expect(sortAsc).toHaveBeenCalled()
+  expect(click).toHaveBeenCalledWith(testProps.value, testProps.direction)
 })
 
 it(`renders correctly and click dsc`, () => {
-  const sortDesc = jest.fn()
+  const testProps = { value: ProjectSearchSortBy.RELEVANCE, direction: DatabaseQueryDirection.DESC }
 
-  const { container } = render(<ColumnSortFilter onSortDesc={() => sortDesc()} />)
+  const { container } = render(<ColumnSortFilter sortValue={testProps.value} currentValue={testProps.value} currentDirection={testProps.direction} onClick={click} />)
 
   const inputElement = screen.getByTestId(`sortDesc`)
   fireEvent.click(inputElement)
 
   expect(container).toMatchSnapshot()
-  expect(sortDesc).toHaveBeenCalled()
+  expect(click).toHaveBeenCalledWith(testProps.value, testProps.direction)
 })
--- src/components/atoms/ProjectSearchBodyHeader/ProjectSearchBodyHeader.spec.tsx
+++ src/components/atoms/ProjectSearchBodyHeader/ProjectSearchBodyHeader.spec.tsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react'
 import { ProjectSearchBodyHeader } from './ProjectSearchBodyHeader'
+import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 
 it(`renders correctly`, () => {
-  const { container } = render(<ProjectSearchBodyHeader />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <ProjectSearchBodyHeader />
+    </TestOvermindWrapper>,
+  )
   expect(container).toMatchSnapshot()
 })
