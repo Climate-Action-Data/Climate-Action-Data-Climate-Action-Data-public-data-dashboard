30c4af0ab8553eaa2107a37d430300ec40614b01
Alexandre Obellianne <aobellianne@palo-it.com>
2023-08-21 17:19:50 +0800

    :bug: fix: fix building issue

--- src/auth-config.ts
+++ src/auth-config.ts
@@ -1,7 +1,7 @@
 export function getAuthConfig() {
   return {
-    domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN,
-    clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID,
-    audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
+    domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ``,
+    clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ``,
+    audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE ?? ``,
   }
 }
