dd8a38fcd79de6b1bfdaf5c5361b917a92ad05a5
Sujay <spsurendran@palo-it.com>
2023-08-21 10:14:32 +0800

    :test_tube: test: added more tests for pdf layout

--- src/app/project/page.pdf.spec.tsx
+++ src/app/project/page.pdf.spec.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { PROJECT_DETAIL_PDF } from '../../test/TestOvermindMockData'
 import { generateProjectPDFDocument } from './page.pdf'
 import * as pdfrenderer from '@react-pdf/renderer'
@@ -16,6 +17,7 @@ jest.mock(`./page.pdf`, () => ({
   ...jest.requireActual(`./page.pdf`),
   projectDetailsPdf: jest.fn().mockReturnValue({}),
 }))
+
 jest.mock(`@react-pdf/renderer`, () => ({
   Font: { register: () => null },
   Document: () => <div>Document</div>,
@@ -30,9 +32,36 @@ jest.mock(`@react-pdf/renderer`, () => ({
     toBlob: () => jest.fn().mockResolvedValue({}),
   })),
 }))
-const pdfFnSpy = jest.spyOn(pdfrenderer, `pdf`)
 
-it(`creates pdf`, async () => {
-  await generateProjectPDFDocument({ projectDetails: PROJECT_DETAIL_PDF })
-  expect(pdfFnSpy).toBeCalledTimes(1)
+const pdfFnSpy = jest.spyOn(pdfrenderer, `pdf`)
+const axiosGetSpy = jest.spyOn(axios, `get`)
+
+describe(`generateProjectPDFDocument`, () => {
+  const mockId = `testid`
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it(`creates pdf from project details`, async () => {
+    await generateProjectPDFDocument({ projectDetails: PROJECT_DETAIL_PDF })
+    expect(pdfFnSpy).toBeCalledTimes(1)
+  })
+
+  it(`creates pdf from project id`, async () => {
+    await generateProjectPDFDocument({ id: mockId })
+    expect(pdfFnSpy).toBeCalledTimes(1)
+    expect(axiosGetSpy).toBeCalledTimes(1)
+  })
+
+  it(`returns without creating pdf when project id and details are empty`, async () => {
+    await generateProjectPDFDocument({})
+    expect(pdfFnSpy).toBeCalledTimes(0)
+  })
+
+  it(`returns without creating pdf when api response is empty`, async () => {
+    axiosGetSpy.mockRejectedValueOnce({ data: {} })
+    await generateProjectPDFDocument({ id: mockId })
+    expect(pdfFnSpy).toBeCalledTimes(0)
+  })
 })
