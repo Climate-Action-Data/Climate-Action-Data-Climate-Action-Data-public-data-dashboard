98cd51c091aead6545d0685ad57d9b348b49d0b2
Burlet Médéric <mburlet@palo-it.com>
2023-08-24 11:11:42 +0800

    :bug: fix(authentication): Removing comment

--- src/components/organisms/Menu/Menu.spec.tsx
+++ src/components/organisms/Menu/Menu.spec.tsx
@@ -1,7 +1,12 @@
 import { render } from '@testing-library/react'
 import { Menu } from './Menu'
+import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 
 it(`renders correctly`, () => {
-  const { container } = render(<Menu />)
+  const { container } = render(
+    <TestOvermindWrapper>
+      <Menu />
+    </TestOvermindWrapper>,
+  )
   expect(container).toMatchSnapshot()
 })
