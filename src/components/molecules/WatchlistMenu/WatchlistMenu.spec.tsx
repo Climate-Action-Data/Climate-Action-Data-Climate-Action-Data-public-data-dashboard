9dac0f658435788db8f3b21d12b1fb7ad4620d3c
Burlet Médéric <mburlet@palo-it.com>
2023-08-24 19:36:03 +0800

    :white_check_mark: fix(test): Fixing coverage

--- src/components/molecules/WatchlistMenu/WatchlistMenu.spec.tsx
+++ src/components/molecules/WatchlistMenu/WatchlistMenu.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { WatchlistMenu } from './WatchlistMenu'
 import { TestRouter } from '@/test/TestRouter'
 import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
@@ -25,5 +25,35 @@ it(`renders correctly and click rename`, () => {
   )
   const renameButton = screen.getByTestId(`watchlist-rename`)
   fireEvent.click(renameButton)
+  expect(container).toMatchSnapshot()
+})
+
+it(`renders correctly and click rename`, () => {
+  const { container } = render(
+    <TestOvermindWrapper>
+      <TestRouter router={{}}>
+        <WatchlistMenu watchlist={watchlistData[0]} />
+      </TestRouter>
+    </TestOvermindWrapper>,
+  )
+  const renameButton = screen.getByTestId(`watchlist-delete`)
+  fireEvent.click(renameButton)
+  waitFor(() => screen.getByTestId(`cancel-delete-modal`))
+  fireEvent.click(screen.getByTestId(`cancel-delete-modal`))
+  expect(container).toMatchSnapshot()
+})
+
+it(`renders correctly and click rename`, () => {
+  const { container } = render(
+    <TestOvermindWrapper>
+      <TestRouter router={{}}>
+        <WatchlistMenu watchlist={watchlistData[0]} />
+      </TestRouter>
+    </TestOvermindWrapper>,
+  )
+  const renameButton = screen.getByTestId(`watchlist-delete`)
+  fireEvent.click(renameButton)
+  waitFor(() => screen.getByTestId(`confirm-delete-modal`))
+  fireEvent.click(screen.getByTestId(`confirm-delete-modal`))
   expect(container).toMatchSnapshot()
 })
