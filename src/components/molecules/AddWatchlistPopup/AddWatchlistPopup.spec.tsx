ec50236f3aebcdf6bf490e5271592369414f75ee
Burlet Médéric <mburlet@palo-it.com>
2023-08-24 19:52:43 +0800

    :white_check_mark: fix(test): Fixing coverage

--- src/components/molecules/AddWatchlistPopup/AddWatchlistPopup.spec.tsx
+++ src/components/molecules/AddWatchlistPopup/AddWatchlistPopup.spec.tsx
@@ -1,6 +1,6 @@
 import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 import { AddWatchlistPopup } from './AddWatchlistPopup'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 
 const PROJECT_ID = `eb21d827-5a9c-43a5-97ff-a9e37e01a72a`
 const MODAL_OPEN = true
@@ -62,5 +62,44 @@ it(`renders correctly and search modal opens and closes`, async () => {
   fireEvent.click(closeButton)
   await new Promise((r) => setTimeout(r, DELAY_FOR_RENDER))
   fireEvent.click(screen.getByTestId(`cancel-modal`))
+  expect(container).toMatchSnapshot()
+})
+
+it(`renders correctly and search modal opens and closes`, async () => {
+  const { container } = render(
+    <TestOvermindWrapper>
+      <AddWatchlistPopup isOpen={MODAL_OPEN} warehouseProjectId={PROJECT_ID} />
+    </TestOvermindWrapper>,
+  )
+  await new Promise((r) => setTimeout(r, DELAY_FOR_RENDER))
+  const closeButton = screen.getByTestId(`search-add-create-modal`)
+  fireEvent.click(closeButton)
+  await waitFor(() => screen.getByTestId(`confirm-modal`))
+  fireEvent.click(screen.getByTestId(`confirm-modal`))
+  expect(container).toMatchSnapshot()
+})
+
+it(`renders correctly and search modal opens and closes`, async () => {
+  const { container } = render(
+    <TestOvermindWrapper>
+      <AddWatchlistPopup isOpen={MODAL_OPEN} warehouseProjectId={PROJECT_ID} />
+    </TestOvermindWrapper>,
+  )
+
+  await waitFor(() => screen.getByTestId(`search-add-watchlist-modal`))
+  fireEvent.input(screen.getByTestId(`search-add-watchlist-modal`), { target: { value: `test` } })
+  expect(container).toMatchSnapshot()
+})
+
+it(`renders correctly and search modal opens and closes`, async () => {
+  const { container } = render(
+    <TestOvermindWrapper>
+      <AddWatchlistPopup isOpen={MODAL_OPEN} warehouseProjectId={PROJECT_ID} />
+    </TestOvermindWrapper>,
+  )
+
+  await waitFor(() => screen.getByTestId(`search-add-watchlist-modal`))
+  fireEvent.input(screen.getByTestId(`search-add-watchlist-modal`), { target: { value: `test` } })
+  fireEvent.input(screen.getByTestId(`search-add-watchlist-modal`), { target: { value: `` } })
   expect(container).toMatchSnapshot()
 })
