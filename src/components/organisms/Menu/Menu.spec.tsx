a12f94847c3e79a6b36e35de4f44e14c1ddf0d03
Alexandre Obellianne <aobellianne@palo-it.com>
2023-08-25 11:08:35 +0800

    :test_tube: test: fix tests

--- src/components/organisms/Menu/Menu.spec.tsx
+++ src/components/organisms/Menu/Menu.spec.tsx
@@ -3,13 +3,26 @@ import { Menu } from './Menu'
 import { TestOvermindWrapper } from '@/test/TestOvermindWrapper'
 import { TestRouter } from '@/test/TestRouter'
 
-it(`renders correctly`, () => {
-  const { container } = render(
-    <TestOvermindWrapper>
-      <TestRouter router={{}}>
-        <Menu />
-      </TestRouter>
-    </TestOvermindWrapper>,
-  )
-  expect(container).toMatchSnapshot()
+import { useAuth0 } from '@auth0/auth0-react'
+
+jest.mock(`@auth0/auth0-react`)
+
+describe(`Menu`, () => {
+  beforeEach(() => {
+    ;(useAuth0 as jest.Mock).mockReturnValue({
+      loginWithRedirect: jest.fn(),
+      getAccessTokenSilently: jest.fn().mockImplementation(() => Promise.resolve(`test`)),
+    })
+  })
+
+  it(`renders correctly`, () => {
+    const { container } = render(
+      <TestOvermindWrapper>
+        <TestRouter router={{}}>
+          <Menu />
+        </TestRouter>
+      </TestOvermindWrapper>,
+    )
+    expect(container).toMatchSnapshot()
+  })
 })
