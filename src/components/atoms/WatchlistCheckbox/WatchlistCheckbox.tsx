0fc0ee8e9fac242f9e57af2bef6220c07e379b0d
Burlet Médéric <mburlet@palo-it.com>
2023-08-25 09:48:13 +0800

    :sparkles: feat(watchlist): toggle and pre-handling

--- src/components/atoms/WatchlistCheckbox/WatchlistCheckbox.tsx
+++ src/components/atoms/WatchlistCheckbox/WatchlistCheckbox.tsx
@@ -2,6 +2,7 @@ import { Watchlist } from '@/@types/Watchlist'
 import { useActions } from '@/overmind'
 import { generateRandomString } from '@/utils/GenerationHelpers'
 import { Box, Flex, Checkbox, Text } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface WatchlistCheckboxProps {
@@ -12,21 +13,30 @@ interface WatchlistCheckboxProps {
 
 export const WatchlistCheckbox = (props: WatchlistCheckboxProps) => {
   const { watchlists, warehouseProjectId, selectedWatchlists } = props
+  const [checkBoxStates, setCheckBoxStates] = useState<Record<string, boolean>>({})
   const { t } = useTranslation(`watchlist`)
   const { addProjectToWatchlist, removeProjectFromWatchlist } = useActions().watchlist
+
+  useEffect(() => {
+    if (watchlists) {
+      const newCheckBoxStates: Record<string, boolean> = {}
+      watchlists.forEach((watchlist) => {
+        newCheckBoxStates[watchlist.id] = selectedWatchlists.includes(watchlist.id)
+      })
+      setCheckBoxStates(newCheckBoxStates)
+    }
+  }, [watchlists])
 
   const handleWatchlistClick = async (event: React.ChangeEvent<HTMLInputElement>, watchlist: Watchlist) => {
     try {
       if (event.target.checked) {
         await addProjectToWatchlist({ warehouseProjectId, watchlistId: watchlist.id })
+        setCheckBoxStates({ ...checkBoxStates, [watchlist.id]: true })
       } else {
         await removeProjectFromWatchlist({ warehouseProjectId, watchlistId: watchlist.id })
+        setCheckBoxStates({ ...checkBoxStates, [watchlist.id]: false })
       }
     } catch (error) {}
-  }
-
-  const shouldBeChecked = (id: string) => {
-    return selectedWatchlists.includes(id)
   }
 
   if (!watchlists || watchlists?.length === 0) {
@@ -38,7 +48,7 @@ export const WatchlistCheckbox = (props: WatchlistCheckboxProps) => {
   } else {
     return watchlists.map((watchlist) => (
       <Box key={`watchlist-check-${generateRandomString()}`} padding="6px 16px">
-        <Checkbox isChecked={shouldBeChecked(watchlist.id)} data-testid="checkbox-watchlist" onChange={(event) => handleWatchlistClick(event, watchlist)}>
+        <Checkbox isChecked={checkBoxStates[watchlist.id]} data-testid="checkbox-watchlist" onChange={(event) => handleWatchlistClick(event, watchlist)}>
           {watchlist.name}
         </Checkbox>
       </Box>
