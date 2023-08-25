8345001906a7abe23f9790a03b5a0860faa455c1
Sujay <spsurendran@palo-it.com>
2023-08-25 12:09:28 +0800

    :test_tube: refactor: update tests

--- src/components/atoms/MenuContent/MenuContent.spec.tsx
+++ src/components/atoms/MenuContent/MenuContent.spec.tsx
@@ -2,16 +2,24 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import { MenuContent, MenuItemProps } from './MenuContent'
 import { DownloadIcon } from '../DownloadIcon/DownloadIcon'
 import { Menu, MenuButton } from '@chakra-ui/react'
+import { BookmarkPlusIcon } from '../BookmarkPlusIcon/BookmarkPlusIcon'
 
 const DEFAULT_BUTTON_TEST = `menu`
 const DEFAULT_BUTTON_TEST_1 = `action 1`
 const DEFAULT_BUTTON_TEST_2 = `action 2`
+const DEFAULT_BUTTON_TEST_3 = `action 3`
 
 describe(`MenuContent`, () => {
   const mockClick = jest.fn()
   const menuList: MenuItemProps[] = [
     { dataTestId: `view-project-details`, onClick: mockClick, text: DEFAULT_BUTTON_TEST_1 },
     { dataTestId: `export-project`, icon: <DownloadIcon />, text: DEFAULT_BUTTON_TEST_2 },
+    {
+      dataTestId: `add-from-watchlist`,
+      text: DEFAULT_BUTTON_TEST_3,
+      icon: <BookmarkPlusIcon />,
+      onClick: mockClick,
+    },
   ]
 
   it(`should render successfully with empty array`, () => {
