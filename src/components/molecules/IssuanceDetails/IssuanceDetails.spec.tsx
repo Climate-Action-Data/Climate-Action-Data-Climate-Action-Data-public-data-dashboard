3731d372b56c523aa97a39e359f38f2d4d543396
Sujay <spsurendran@palo-it.com>
2023-08-31 10:22:29 +0800

    :test_tube: refactor: tests for unit tags empty

--- src/components/molecules/IssuanceDetails/IssuanceDetails.spec.tsx
+++ src/components/molecules/IssuanceDetails/IssuanceDetails.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react'
 import { IssuanceDetails } from './IssuanceDetails'
 import { issuanceDetail } from '@/test/mock-data/issuance_detail'
+import { Issuance } from '@/@types/Issuance'
 
 it(`renders correctly with no project`, () => {
   const { container } = render(<IssuanceDetails issuance={undefined} />)
@@ -9,5 +10,11 @@ it(`renders correctly with no project`, () => {
 
 it(`renders correctly with validation`, () => {
   const { container } = render(<IssuanceDetails issuance={issuanceDetail} />)
+  expect(container).toMatchSnapshot()
+})
+
+it(`renders correctly without tags`, () => {
+  const issuanceDetailWithoutTags: Issuance = { ...issuanceDetail, issuanceTags: undefined }
+  const { container } = render(<IssuanceDetails issuance={issuanceDetailWithoutTags} />)
   expect(container).toMatchSnapshot()
 })
