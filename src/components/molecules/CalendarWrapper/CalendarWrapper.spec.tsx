ccde43c78287a10f5904de179e43914f852502e9
Abel Lim <alim@palo-it.com>
2023-10-02 14:50:23 +0800

    :lipstick: style: Update formatting of project issuance page

--- src/components/molecules/CalendarWrapper/CalendarWrapper.spec.tsx
+++ src/components/molecules/CalendarWrapper/CalendarWrapper.spec.tsx
@@ -69,8 +69,8 @@ describe(`CalendarWrapper`, () => {
 
     const { container } = render(<CalendarWrapper onApplySelectedDate={tApplySelectedDate} preSelectedDate={tDate} />)
 
-    const tPrevMonth = screen.getByLabelText(`before Sep`)
-    const tNextMonth = screen.getByLabelText(`after Sep`)
+    const tPrevMonth = screen.getByLabelText(`before Oct`)
+    const tNextMonth = screen.getByLabelText(`after Oct`)
     const tPrevYear = screen.getByLabelText(`before 2023`)
     const tNextYear = screen.getByLabelText(`after 2023`)
 
