e8cd36bc76225786a7c03008cec70a8f9129193b
Abel Lim <alim@palo-it.com>
2023-10-11 16:55:55 +0800

    :lipstick: style: Set minimum height to allow elements to push down the Search Body

--- src/components/molecules/SearchHeader/SearchHeader.tsx
+++ src/components/molecules/SearchHeader/SearchHeader.tsx
@@ -72,7 +72,17 @@ export const SearchHeader = (props: SearchHeaderProps) => {
   }
 
   return (
-    <Flex id="headerReference" position="sticky" top={`56px`} zIndex="docked" padding="16px 24px" minH={[`430px`, `184px`]} color="white" backgroundColor="gray.900" width="100%">
+    <Flex
+      id="headerReference"
+      position="sticky"
+      top={`56px`}
+      zIndex="docked"
+      padding="16px 24px"
+      minH={[`430px`, null, null, `184px`]}
+      color="white"
+      backgroundColor="gray.900"
+      width="100%"
+    >
       <VStack alignItems="start" flex={1}>
         {currentPath && <BreadCrumbs items={[{ title: currentTitle, link: currentPath }]} />}
         <HStack w={`100%`} justifyContent={`space-between`}>
